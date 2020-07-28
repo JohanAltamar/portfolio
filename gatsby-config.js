93d660a81d8f88eb4bfb47d7c7cb325cc5742c59
Johan Altamar <johan.altamar.dev@gmail.com>
2020-07-28 17:24:01 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,24 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Johan Altamar | Web Developer`,
+    description: `Web development and embedded systems in the same place.`,
+    author: `Johan Altamar`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Johan Dev`,
+        short_name: `Johan Dev`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
 }
